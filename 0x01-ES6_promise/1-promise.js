2297d5a888604de5cef5b7536e6da79da2c62d64
A-fethi <fethiabderrahmane1@gmail.com>
2024-01-03 12:23:35 +0000

    Fixing requirements

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 function getResponseFormatAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({status: 200, body: 'Success'});
+      resolve({ status: 200, body: 'Success' });
     } else {
       reject(Error('The fake API is not working currently'));
     }
